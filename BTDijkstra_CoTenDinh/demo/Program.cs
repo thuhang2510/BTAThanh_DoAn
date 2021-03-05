9743d8a8ca7e99bbb3e3719f81083fcc489b439d
thuhang2510 <tunguyet2510@gmail.com>
2021-03-05 19:38:59 +0700

    Sua lai Program.cs

--- BTDijkstra_CoTenDinh/demo/Program.cs
+++ BTDijkstra_CoTenDinh/demo/Program.cs
@@ -8,13 +8,22 @@ namespace demo
 {
     class Program
     {
+        enum LoaiXe
+        {
+            oto,
+            xemay,
+            ca2
+        }
+
         static void Main(string[] args)
         {
             FileIO fileIO = new FileIO();
-            Input input = fileIO.docFile("input.txt");
+            string[] tenFile = { "input_trongso.txt", "input_tenduongdi.txt", "input_loaixe.txt", "input_tenDinh.txt" };
+            Input input = fileIO.docFile(tenFile);
 
             Dijkstra dijkstra = new Dijkstra(input.g, input.vertexName);
-            List<string> kq = dijkstra.findPath("Kieu_Ky", "Ngoc_Dong", 1);
+
+            List<string> kq = dijkstra.findPath("Hung_Yen", "Yen_Bai", (int)LoaiXe.xemay);
 
             if (kq == null)
                 Console.WriteLine("Khong co duong di");
