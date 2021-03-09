90aede10ef5ecc7a639318d612686e4539c50485
thuhang2510 <tunguyet2510@gmail.com>
2021-03-09 16:34:45 +0700

    Lam lai FileIO.cs

--- BTDijkstra_CoTenDinh/demo/FileIO.cs
+++ BTDijkstra_CoTenDinh/demo/FileIO.cs
@@ -3,64 +3,41 @@ using System.IO;
 
 class FileIO
 {
-    private List<string[]> readFileList(string[] fileName)
-    {
-        List<string[]> fileList = new List<string[]>();
-
-        foreach (string name in fileName)
-            fileList.Add(File.ReadAllLines(name));
-
-        return fileList;
-    }
-
-    private List<string[]> splitListStr(List<string[]> fileList, int line)
+    private string[] splitListStr(string fileLine)
     {
         char[] sep = new char[] { ' ', '\t' };
 
-        List<string[]> listStr = new List<string[]>();
-
-        foreach (string[] str in fileList)
-            listStr.Add(str[line].Trim().Split(sep, System.StringSplitOptions.RemoveEmptyEntries));
-
-        return listStr;
+        return fileLine.Trim().Split(sep, System.StringSplitOptions.RemoveEmptyEntries);
     }
 
-    private (int weight, int vehicle, string nameRoad) getValue(List<string[]> fileList, int vt)
+    public Input docFile()
     {
-        List<string> arrValue = new List<string>();
+        string[] fileTrongSo = File.ReadAllLines("input_trongso.txt");
+        string[] fileTenDinh = File.ReadAllLines("input_tenduongdi.txt");
+        string[] fileTenDuong = File.ReadAllLines("input_loaixe.txt");
+        string[] fileLoaiXe = File.ReadAllLines("input_tenDinh.txt");
 
-        for (int i = 0; i < fileList.Count - 1; ++i)
-            arrValue.Add(giatri(fileList[i], vt));
-
-        return (int.Parse(arrValue[0]), int.Parse(arrValue[2]), arrValue[1]);
-    }
-
-    private string giatri(string[] list, int vt)
-    {
-        return list[vt];
-    }
-
-    public Input docFile(string[] fileName)
-    {
-        List<string[]> fileList = readFileList(fileName);
-
-        int n = int.Parse(fileList[0][0]);
-
+        int n = int.Parse(fileTrongSo[0]);
         List<string> vertexName = new List<string>();
         Graph g = new Graph(n);
 
-        for (int i = 1; i < fileList[0].Length; ++i)
+        for (int i = 1; i < fileTrongSo.Length; ++i)
         {
-            List<string[]> listStr = splitListStr(fileList, i);
+            string[] arrTS = splitListStr(fileTrongSo[i]);
+            string[] arrTenDinh = splitListStr(fileTenDinh[i]);
+            string[] arrTenDuong = splitListStr(fileTenDuong[i]);
+            string[] arrLoaiXe = splitListStr(fileLoaiXe[i]);
 
             for (int j = 0; j < n; ++j)
             {
-                (int weight, int vehicle, string nameRoad) = getValue(listStr, j);
+                int weight = int.Parse(arrTS[j]);
+                int vehicle = int.Parse(arrLoaiXe[j]);
+                string nameRoad = arrTenDuong[j];
 
                 g[i - 1, j] = new Road(weight, vehicle, nameRoad);
             }
 
-            vertexName.Add(giatri(listStr[3], 1));
+            vertexName.Add(arrTenDinh[1]);
         }
 
         Input input = new Input(vertexName, g);
